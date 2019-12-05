d1bbcfe7265d1c8529a21231679cd5dbe72dc690
Claudio <claudio.junior14@hotmail.com>
2019-12-05 17:06:32 -0300

    Frontend init and README repo

---
+++ README.md
@@ -0,0 +1,17 @@
+# Test to Target Job Application
+
+## Frontend (Vuejs)
+
+```bash
+cd frontend
+yarn 
+yarn serve
+```
+
+## Backend (NodeJs + MongoDB)
+
+```bash
+cd backend
+yarn
+yarn dev
+```
---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col align-center cols="12">
+          <span>teste</span>
+        </v-col>
+      </v-row>
+      <div id="app">
+        <div id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
+        <router-view />
+      </div>
+    </v-container>
+  </v-app>
+</template>
